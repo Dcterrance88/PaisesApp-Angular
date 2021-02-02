b6a0b517a1917aa91f9aafde85761afe505e6b52
Dcterrance88 <>
2021-02-01 21:47:48 -0500

    Llenando la tabla de paises
    por medio de la respuesta se lleno la tabla de paises y se manejaron
    algunas condiciones para que no muestre la tabla cuando no se ha buscado
    nada.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,6 +12,7 @@ export class PorPaisComponent {
 
   public termino: string = '';
   public hayError: boolean = false;
+  public paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
@@ -19,10 +21,12 @@ export class PorPaisComponent {
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
-      .subscribe( (paises) => {
-        console.log(paises);
+      .subscribe( (paisesResp) => {
+        this.paises = paisesResp;
+        console.log(this.paises);
       }, (err) => {
         this.hayError = true;
+        this.paises = [];
       });
   }
 
