f498002ffb13d7acd82d22230f72b3bd8f3e2504
Dcterrance88 <>
2021-02-02 15:19:41 -0500

    Terminando la pantalla de ver pais
    se añadio estilos e informacion al html del componente ver pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -11,6 +13,8 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  public pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService:PaisService) { }
@@ -18,29 +22,14 @@ export class VerPaisComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id))
+        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id)),
+        tap( console.log )
       )
-      .subscribe( resp => {
-        console.log(resp)
-      });
-    // this.activatedRoute.params
-    //   .subscribe( ({ id }) => {
-    //     this.paisService.getPaisPorAlpha( id )
-    //       .subscribe(pais =>{
-    //         console.log(pais);
-    //       })
-    //   })
+      .subscribe( pais => this.pais = pais);
   }
 
 }
-
 /*
-El ActivatedRoute viene con todo lo necesario para suscribirnos a cualquier cambio de la url.
-para hacerlo en el on init usamos la propiedad que inyectamos, llamamos al metodo params el cual
-es un observable, asi que vamos a suscribirnos y con ello obtenemos el parametro de la ruta definida
-con id pero dentro de el tenemos que hacer otro observable para traer la informacion del pais
-
-el operador switchMap te permite recibir un observable y regresar otro observable, en este ejemplo
-esta retornando el observable del servicio dando como resultado los datos del coutry ya que se le esta pasando
-el id y cuando se suscribe lo hace con el observable retornado
+el tap es un operador que dispara un efecto secundario en este caso cuando le pasamos por ejemplo un 
+console log, imprimira el resultado de ese observable
 */
