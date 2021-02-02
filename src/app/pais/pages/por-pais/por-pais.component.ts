42a61d42d0098d7078d112102586e02bed7d3855
Dcterrance88 <>
2021-02-01 20:41:40 -0500

    Manejo de errores
    en el pais component en el servicio inyectado se suscribe y maneja el
    error haciendo que en el pais component html muestre un mensaje de error
    y oculte la tabla.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,15 +10,20 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   public termino: string = '';
+  public hayError: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   public buscar(): void {
+    this.hayError = false;
     console.log(this.termino);
+    
     this.paisService.buscarPais(this.termino)
-    .subscribe(resp =>{
-      console.log(resp);
-    });
+      .subscribe((resp) => {
+        console.log(resp);
+      }, (err) => {
+        this.hayError = true;
+      });
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -15,7 +15,7 @@ export class PaisService {
 
     const url = `${this.apiUrl}/name/${ termino }`;
     return this.http.get(url);
-    
+
   }
 
 
